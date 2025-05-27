e4bebc37be75aec84e2c8b3ac81326fec58a360b
mustafa egeh <Mr.mustafaegeh@gmail.com>
2025-05-27 21:00:08 +0300

    updated js and form errors

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const nav = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+function showAlert() {
+  document.getElementById("confirmation-message").innerHTML = alert(
+    "The Form Has Been Submited"
+  );
+}
+
+function jobInfo() {
+  document.getElementById("job-des-page").innerHTML = alert(
+    "The Job is Not Yet Avaliable "
+  );
+}
